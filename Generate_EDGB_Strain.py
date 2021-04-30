f785bb6d7826223bf13d4fd451025de3f603142b
Maria Okounkova <m.okounkova@gmail.com>
2021-04-29 17:10:33 -0700

    Generate_EDGB_Strain.py: script for generating EDGB-modified waveform

--- Generate_EDGB_Strain.py
+++ Generate_EDGB_Strain.py
@@ -6,16 +6,14 @@ import scipy
 import h5py
 import numpy as np
 import os
-sys.path.insert(1, 'catalog_tools')
-from convert_sxs_to_lvc import *
 
-def ReadExtrapolatedMode(p, piece, mode, lev, ell=None):
+def ReadExtrapolatedMode(p, piece, mode, sqrt_alpha=None):
   """ Given a file of extrapolated modes, read in the (mode)
       at a given order """
-  ell = str(ell).replace('.', 'p')
-  piece_dict = {"DeltaStrain" : "Lev" + str(lev) + "/DeltaStrain.h5", \
-          "BackgroundStrain" : "Lev" + str(lev) + "/BackgroundStrain.h5", \
-          "dCSModified" : "Lev" + str(lev) + "/dCS_Strain_" + ell + ".h5"}
+  sqrt_alpha = str(sqrt_alpha).replace('.', 'p')
+  piece_dict = {"DeltaStrain" : "DeltaStrain.h5", \
+          "BackgroundStrain" : "BackgroundStrain.h5", \
+          "EDGBModified" : "EDGB_Strain_" + sqrt_alpha + ".h5"}
   file = p + piece_dict[piece]
   l = mode[0]
   m = mode[1]
@@ -25,13 +23,13 @@ def ReadExtrapolatedMode(p, piece, mode, lev, ell=None):
   result = re + 1j*im
   return time, result
 
-def ComputedCSModifiedStrain(p, mode, l, lev):
-  """ Given a value of the dCS coupling constant l, a path 
+def ComputeEDGBModifiedStrain(p, mode, l):
+  """ Given a value of the EDGB coupling constant l, a path 
       p to the extrapolated hPsi4 compute the modified gravitational wave strain """
 
   ## Read in the background
-  time, strain = ReadExtrapolatedMode(p, "BackgroundStrain", mode, lev)
-  delta_time, delta_strain = ReadExtrapolatedMode(p, "DeltaStrain", mode, lev)
+  time, strain = ReadExtrapolatedMode(p, "BackgroundStrain", mode)
+  delta_time, delta_strain = ReadExtrapolatedMode(p, "DeltaStrain", mode)
 
   ## Now add the strain and delta strain together
   ## with the correct value of l
@@ -39,16 +37,16 @@ def ComputedCSModifiedStrain(p, mode, l, lev):
 
   return time, total
 
-def OutputdCSModifiedStrain(p, ell, only22, dropm0, lev):
+def OutputEDGBModifiedStrain(p, sqrt_alpha, only22, dropm0):
     """ Generate an h5 file with the modified strain for a 
-        given value of ell """
+        given value of sqrt_alpha """
 
     ## For naming the file, replace . with p because otherwise
     ## the .h5 file can't be read by catalog scripts
-    name = str(ell).replace('.', 'p')
+    name = str(sqrt_alpha).replace('.', 'p')
 
     ## Make the output file
-    OutFile = p + "Lev" + str(lev) + "/dCS_Strain_" + name + ".h5"
+    OutFile = p + "/EDGB_Strain_" + name + ".h5"
     fOut = h5py.File(OutFile, 'w')
     
     grp = fOut.create_group("Extrapolated_N2.dir")
@@ -62,7 +60,7 @@ def OutputdCSModifiedStrain(p, ell, only22, dropm0, lev):
         l = mode[0]
         m = mode[1]
         print("Computing for ", mode)
-        time, total = ComputedCSModifiedStrain(p, mode, ell, lev)
+        time, total = ComputeEDGBModifiedStrain(p, mode, sqrt_alpha)
         ## Compute for the given mode
         dataset = grp.create_dataset("Y_l"+str(l)+"_m"+str(m)+".dat", \
         (len(time),3), dtype='f')
@@ -102,7 +100,7 @@ def OutputdCSModifiedStrain(p, ell, only22, dropm0, lev):
         
                 if m != 0:
                     ## Compute the mode if not m = 0
-                    time, total = ComputedCSModifiedStrain(p, mode, ell, lev)
+                    time, total = ComputeEDGBModifiedStrain(p, mode, sqrt_alpha)
 
                     dataset = grp.create_dataset("Y_l"+str(l)+"_m"+str(m)+".dat", \
                     (len(time),3), dtype='f')
@@ -134,7 +132,7 @@ def OutputdCSModifiedStrain(p, ell, only22, dropm0, lev):
                 print(mode)
 
                 ## Compute for the given mode
-                time, total = ComputedCSModifiedStrain(p, mode, ell, lev)
+                time, total = ComputeEDGBModifiedStrain(p, mode, sqrt_alpha)
 
                 dataset = grp.create_dataset("Y_l"+str(l)+"_m"+str(m)+".dat", \
                 (len(time),3), dtype='f')
@@ -155,20 +153,20 @@ def GetModesFromString(modes):
         modes = [[2, 2]]
     return modes
 
-def GenerateStrainFiles(ell, lev, only22, dropm0):
-    """ Generates the sxs format waveform and LVC format waveform for a given
-        beyond-GR simulation with coupling parameter ell. 
+def GenerateStrainFiles(p, sqrt_alpha, only22, dropm0):
+    """ Generates the sxs format waveform for a given
+        beyond-GR simulation with coupling parameter sqrt_alpha. 
         
-        The file BeyondGRAnalysis/Waveforms/Lev*/BackgroundStrain.h5 contains the strain
+        The file BackgroundStrain.h5 contains the strain
         of the GR background, h_GR. 
         
-        Meanwhile the file BeyondGRAnalysis/Waveforms/Lev*/DeltaStrain.h5 contains the 
+        Meanwhile the file DeltaStrain.h5 contains the 
         leading-order modification to the beyond-GR strain, delta_h. 
         
         We first generate the sxs format total beyond-GR waveform for these
         using 
         
-        h = h_GR + ell^4 * delta_h. 
+        h = h_GR + sqrt_alpha^4 * delta_h. 
         
         Then we convert the resulting waveform into the LVC format, from 
         which we can then generate frames files
@@ -177,31 +175,30 @@ def GenerateStrainFiles(ell, lev, only22, dropm0):
         which can be useful for testing purposes
         
         """
-    ## Since ell has a decimal point and *.h5 readers cannot
+    ## Since sqrt_alpha has a decimal point and *.h5 readers cannot
     ## handle this decimal point, we will replace the decimal point 
     ## with the character p, ie
     ## 0.1 will become 0p1
-    ell_string = str(ell).replace('.', 'p')
+    sqrt_alpha_string = str(sqrt_alpha).replace('.', 'p')
 
     ## Call to generate total waveform in sxs format
-    OutputdCSModifiedStrain("Waveforms/", ell, only22, dropm0, lev)
+    OutputEDGBModifiedStrain(p + "/", sqrt_alpha, only22, dropm0)
     
 def main():
-  p = argparse.ArgumentParser(description="Generate dCS waveform for a given coupling parameter value")
-  p.add_argument("--ell", required=True, type=float,\
-    help="Value of dCS coupling constant")
-  p.add_argument("--lev", required=True, type=int,\
-    help="Resolution of NR run to use")
+  p = argparse.ArgumentParser(description="Generate EDGB waveform for a given coupling parameter value")
+  p.add_argument("--dir", required=True, \
+    help="Directory contraining the BackgroundStrain.5 and DeltaStrain.h5 files")
+  p.add_argument("--sqrt_alpha", required=True, type=float,\
+    help="Value of EDGB coupling constant")
   p.add_argument('--only22', help='Only output the 22 mode', \
     dest='only22', action='store_true')
   p.add_argument('--dropm0', help='Include all modes up to l = 8 except m = 0 modes', \
     dest='dropm0', action='store_true')
   p.set_defaults(only22=False)
   p.set_defaults(dropm0=False)
-  p.set_defaults(convert_lvc=False)
   args = p.parse_args()
 
-  GenerateStrainFiles(args.ell, args.lev, args.only22, args.dropm0)
+  GenerateStrainFiles(args.dir, args.sqrt_alpha, args.only22, args.dropm0)
 
 if __name__ == "__main__":
   main()
