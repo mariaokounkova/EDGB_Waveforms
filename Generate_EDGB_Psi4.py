b835801d15f35099c15b177a0a2c55392ace8f7d
Maria Okounkova <m.okounkova@gmail.com>
2023-03-05 12:10:47 -0800

    EdGB waveform processing and plotting

---
+++ Generate_EDGB_Psi4.py
@@ -0,0 +1,176 @@
+#!/usr/bin/python
+
+import sys
+import argparse
+import scipy
+import h5py
+import numpy as np
+import os
+
+def ReadExtrapolatedMode(p, piece, mode):
+  """ Given a file of extrapolated modes, read in the (mode)
+      at a given order """
+  piece_dict = {"Delta" : "rMDeltaPsi4_Asymptotic_GeometricUnits.h5", \
+          "Background" : "rMPsi4_Asymptotic_GeometricUnits.h5"}
+  file = p + piece_dict[piece]
+  l = mode[0]
+  m = mode[1]
+  f = h5py.File(file, 'r')
+  data = f['Extrapolated_N2.dir']['Y_l' + str(l) + '_m'  + str(m) + '.dat']
+  time, re, im = data[:,0], data[:,1], data[:,2]
+  result = re + 1j*im
+  return time, result
+
+def ComputeEDGBPsi4(p, mode, sqrt_alpha):
+  """ Combine the background and delta psi4"""
+
+  ## Read in the background
+  time, strain = ReadExtrapolatedMode(p, "Background", mode)
+  delta_time, delta_strain = ReadExtrapolatedMode(p, "Delta", mode)
+  print(time[-6:])
+  print(delta_time[-6:])
+
+  ## Now add the strain and delta strain together
+  ## with the correct value of sqrt_alpha
+  total = strain + sqrt_alpha**4 * delta_strain
+
+  return time, total
+
+def OutputEDGBPsi4(p, sqrt_alpha):
+    """ Generate an h5 file with the modified psi4 for a 
+        given value of sqrt_alpha """
+
+    ## For naming the file, replace . with p because otherwise
+    ## the .h5 file can't be read by catalog scripts
+    name = str(sqrt_alpha).replace('.', 'p')
+
+    ## Make the output file
+    OutFile = p + "/EDGB_Psi4_" + name + ".h5"
+    fOut = h5py.File(OutFile, 'w')
+    
+    grp = fOut.create_group("Extrapolated_N2.dir")
+    
+    ### Compute for only (2,2) modes
+    #if only22:
+    #  print("Computing for (2,2) mode only")
+
+    #  modes = [(2, -2), (2,2)]
+    #  for mode in modes:
+    #    l = mode[0]
+    #    m = mode[1]
+    #    print("Computing for ", mode)
+    #    time, total = ComputeEDGBModifiedStrain(p, mode, sqrt_alpha)
+    #    ## Compute for the given mode
+    #    dataset = grp.create_dataset("Y_l"+str(l)+"_m"+str(m)+".dat", \
+    #    (len(time),3), dtype='f')
+
+    #    dataset[:,0] = time
+    #    dataset[:,1] = np.real(total)
+    #    dataset[:,2] = np.imag(total)
+
+    #  ## For all other modes
+    #  l_arr = range(2, 9)
+    #  for l in l_arr:
+    #    print("Computing for l = ", l)
+
+    #    for m in range(-l, l+1):
+    #      mode = (l, m)
+    #      if mode not in modes:
+    #        print("Setting zero for ", mode)
+    #        dataset = grp.create_dataset("Y_l"+str(l)+"_m"+str(m)+".dat", \
+    #        (len(time),3), dtype='f')
+
+    #        dataset[:,0] = np.zeros_like(time)
+    #        dataset[:,1] = np.zeros_like(time)
+    #        dataset[:,2] = np.zeros_like(time)
+            
+    ## All modes except m = 0
+    #elif dropm0:
+    #    print("Computing for all modes except m = 0")
+    #
+    #    l_arr = range(2, 9)
+
+    #    for l in l_arr:
+    #        print("Computing for l = ", l)
+
+    #        for m in range(-l, l+1):
+    #        
+    #            mode = (l, m)
+    #    
+    #            if m != 0:
+    #                ## Compute the mode if not m = 0
+    #                time, total = ComputeEDGBModifiedStrain(p, mode, sqrt_alpha)
+
+    #                dataset = grp.create_dataset("Y_l"+str(l)+"_m"+str(m)+".dat", \
+    #                (len(time),3), dtype='f')
+
+    #                dataset[:,0] = time
+    #                dataset[:,1] = np.real(total)
+    #                dataset[:,2] = np.imag(total)
+    #        
+    #            else:
+    #                ## Set to zero if m = 0 
+    #                print("Setting zero for ", mode)
+    #                dataset = grp.create_dataset("Y_l"+str(l)+"_m"+str(m)+".dat", \
+    #                (len(time),3), dtype='f')
+
+    #                dataset[:,0] = np.zeros_like(time)
+    #                dataset[:,1] = np.zeros_like(time)
+    #                dataset[:,2] = np.zeros_like(time)
+        
+    ## Compute for all of the modes
+    print("Computing for all of the modes")
+    l_arr = [2] ##range(2, 9)
+
+    for l in l_arr:
+        print("Computing for l = ", l)
+
+        for m in [2]: ##range(-l, l+1):
+            mode = (l, m)
+            print(mode)
+
+            ## Compute for the given mode
+            time, total = ComputeEDGBPsi4(p, mode, sqrt_alpha)
+
+            dataset = grp.create_dataset("Y_l"+str(l)+"_m"+str(m)+".dat", \
+            (len(time),3), dtype='f')
+
+            dataset[:,0] = time
+            dataset[:,1] = np.real(total)
+            dataset[:,2] = np.imag(total)
+
+    fOut.close()
+    print("Wrote waveforms to file", OutFile)
+
+def GenerateEDGBPsi4Files(p, sqrt_alpha):
+    """ Generates the sxs format waveform for a given
+        beyond-GR simulation with coupling parameter sqrt_alpha. 
+        """
+
+    ## Since sqrt_alpha has a decimal point and *.h5 readers cannot
+    ## handle this decimal point, we will replace the decimal point 
+    ## with the character p, ie
+    ## 0.1 will become 0p1
+    sqrt_alpha_string = str(sqrt_alpha).replace('.', 'p')
+
+    ## Call to generate total waveform in sxs format
+    OutputEDGBPsi4(p + "/", sqrt_alpha)
+    
+def main():
+  p = argparse.ArgumentParser(description="Generate EDGB waveform for a given coupling parameter value")
+  p.add_argument("--dir", required=True, \
+    help="Directory containing the Background and Delta Psi4 files")
+  p.add_argument("--sqrt_alpha", required=True, type=float,\
+    help="Value of EDGB coupling constant")
+  #p.add_argument('--only22', help='Only output the 22 mode', \
+  #  dest='only22', action='store_true')
+  #p.add_argument('--dropm0', help='Include all modes up to l = 8 except m = 0 modes', \
+  #  dest='dropm0', action='store_true')
+  #p.set_defaults(only22=False)
+  #p.set_defaults(dropm0=False)
+  args = p.parse_args()
+
+  GenerateEDGBPsi4Files(args.dir, args.sqrt_alpha)
+
+if __name__ == "__main__":
+  main()
--- Processing/Process_EDGB_Waveforms.py
+++ Processing/Process_EDGB_Waveforms.py
@@ -10,8 +10,8 @@ import json
 import sxs
 from scipy.optimize import fmin
 
-""" Given extrapolated waveforms for the strain and delta psi4, 
-    compute BackgroundStrain and DeltaStrain """
+""" Given extrapolated waveforms for the psi4 and delta psi4, 
+    compute BackgroundPsi4 and DeltaPsi4 """
 
 def CutTimes(time, data, TLow, TUp): 
   """ Cut time and data to be between 
@@ -32,26 +32,6 @@ def SubtractPeakTimeMode(time, data):
   t_peak = GetPeakTimeMode(time, data)
   return time - t_peak
 
-def InterpolateTimes(time, data, time_dest):
-    """ Interpolates time, data onto new time axis
-        time_dest """
-    ## build the interpolant only in the region where we need it
-    ## time, data = CutTimes(time, data, min(time_dest), max(time_dest))
-    interpolant = scipy.interpolate.CubicSpline(time, data)
-    return interpolant(time_dest)
-
-def RunningIntegral(time, data):
-    """ Given an array, compute an integral of the array between
-        the start time and each point in time"""
-    ## Go over each time and compute the integral to that time
-    integral = np.zeros(len(time))
-    for n in range(1, len(time)):
-        t = time[0:n]
-        d = data[0:n]
-        integ = scipy.integrate.simps(d, x=t)
-        integral[n] = integ
-    return time, integral
-
 def DeltaPsi4Factor(hPsi4, B = 0.1):
     """ Given DeltaPsi4 
 
@@ -69,11 +49,11 @@ def DeltaPsi4Factor(hPsi4, B = 0.1):
     hPsi4 = hPsi4 / B**2
     return hPsi4
 
-def ReadExtrapolatedMode(p, piece, mode):
+def ReadExtrapolatedMode(p, piece, mode = [2,2]):
   """ Given a file of extrapolated modes, read in the (mode)
       at a given order """
-  piece_dict = {"hRWZ" : "/rhOverM_Extrapolated_N2.h5", \
-          "DeltaPsi4" : "/rMDeltaPsi4_Extrapolated_N2.h5"}
+  piece_dict = {"Psi4" : "/rMPsi4_Asymptotic_GeometricUnits.h5", \
+          "DeltaPsi4" : "/rMDeltaPsi4_Asymptotic_GeometricUnits.h5"}
   try:
     file = p + piece_dict[piece]
   except: file = p + piece
@@ -88,61 +68,19 @@ def ReadExtrapolatedMode(p, piece, mode):
     result = DeltaPsi4Factor(result)
   return time, result
 
-def ChristodolouMass(data_dir):
-  """ Grab the sum of the Christodolou masses from 
-      metadata.json file """
-  with open(data_dir + 'metadata.json') as json_file:
-    data = json.load(json_file)
-    m1 = data['reference_mass1']
-    m2 = data['reference_mass2']
-    return m1 + m2
+def ComputeEDGBPsi4(p, mode):
+  """ Given a path p to the extrapolated DeltaPsi4, 
+      compute the modification to the gravitational wave psi4. 
+      Return the time and modified Psi4 """
 
-def ComputeStrain(time, psi4, data_dir):
-    """ Compute the delta strain from a psi4 -- 
-        could be background psi4, or delta psi4, or 
-        background psi4 + l**4 * delta psi4. 
-        Integrate both the real and imag parts 
-        Have to do each part separately because scipy integrate doesn't 
-        understand integrating a complex number. Luckily integration is a 
-        linear operation """
-    ## Grab the sum of the Christodolou masses
+  ## Grab the background psi4
+  time, psi4 = ReadExtrapolatedMode(p, "Psi4", mode)
 
-    mass = 1.0 ## TODO TEMP FOR NOW ChristodolouMass(data_dir) 
-    ## Divide the result twice by the Christodolou mass, since we 
-    ## have rDeltaMPsi4, and we need rhOverM
-    psi4_real = np.real(psi4)
-    psi4_imag = np.imag(psi4)
-
-    news_real_time, news_real = RunningIntegral(time, psi4_real)
-    news_imag_time, news_imag = RunningIntegral(time, psi4_imag)
-
-    strain_real_time, strain_real = RunningIntegral(news_real_time, news_real)
-    strain_imag_time, strain_imag = RunningIntegral(news_imag_time, news_imag)
-
-    strain = (strain_real + 1j*strain_imag)/ mass**2
-    return strain_real_time, strain
-
-def ComputeEDGBDeltaStrain(p, mode):
-  """ Given a path p to the extrapolated hPsi4, 
-      compute the modification to the gravitational wave strain. 
-      Return time time, strain, and delta_strain """
-
-  print("Computing delta strain for " + str(mode) + "\n")
+  print("Grabbing delta psi4 for " + str(mode) + "\n")
   ## Grab delta psi4
   delta_time, delta_psi4 = ReadExtrapolatedMode(p, "DeltaPsi4", mode)
 
-  ## Grab the background strain
-  time, strain = ReadExtrapolatedMode(p, "hRWZ", mode)
-
-  ## Compute delta strain
-  delta_time, delta_strain = ComputeStrain(delta_time, delta_psi4, p)
-  
-  ## First check that we do in fact start delta_strain at zero
-  if(delta_strain[0] != 0):
-    print("Waveform perturbation is initially non-zero!")
-    return
-
-  ## Pad the delta strain array to be the length of the simulation
+  ## Pad the delta psi4 array to be the length of the simulation
   delta_start_time = delta_time[0]
 
   ## Cut the time array
@@ -150,27 +88,26 @@ def ComputeEDGBDeltaStrain(p, mode):
   time_after  = time[np.where(time >= delta_start_time)]
 
   ## Make an array of zeroes for the time before
-  delta_strain_pad = np.zeros(len(time_before))
+  delta_psi4_pad = np.zeros(len(time_before))
 
   ## Now add in the cut array to the delta array
   delta_time = np.concatenate((time_before, delta_time))
-  delta_strain = np.concatenate((delta_strain_pad, delta_strain))
+  delta_psi4 = np.concatenate((delta_psi4_pad, delta_psi4))
 
   ## Now working with the padded array
   ## Interpolate onto the perturbed time axis
-  #strain = InterpolateTimes(time, strain, delta_time)
-  delta_strain = InterpolateTimes(delta_time, delta_strain, time)
+  delta_psi4 = InterpolateTimes(delta_time, delta_psi4, time)
 
-  return time, strain, delta_strain
+  return time, psi4, delta_psi4
   
-def OutputEDGBDeltaStrain(p, only22):
+def OutputEDGBPsi4(p, only22):
   """ Given a path p to the extrapolated hPsi4, 
-      compute the modification to the gravitational wave strain. 
-      Return time time, strain, and delta_strain """
+      compute the modification to the gravitational wave psi4,
+      Return time, psi4, and delta psi4 """
 
   ## Dump the result to a file
-  OutFile1 = p + 'BackgroundStrain.h5'
-  OutFile2 = p + 'DeltaStrain.h5'
+  OutFile1 = p + 'BackgroundPsi4.h5'
+  OutFile2 = p + 'DeltaPsi4.h5'
 
   fOut1 = h5py.File(OutFile1, 'w')
   fOut2 = h5py.File(OutFile2, 'w')
@@ -186,27 +123,22 @@ def OutputEDGBDeltaStrain(p, only22):
 
       mode = (l, m)
 
-      ## Compute strain and delta strain for this mode
-      time, strain, delta_strain = ComputeEDGBDeltaStrain(p, mode)
+      ## Compute psi4 and delta psi4 for this mode
+      time, psi4, delta_psi4 = ComputeEDGBPsi4(p, mode)
 
       dataset1 = grp1.create_dataset("Y_l"+str(l)+"_m"+str(m)+".dat", \
         (len(time),3), dtype='f')
       dataset2 = grp2.create_dataset("Y_l"+str(l)+"_m"+str(m)+".dat", \
         (len(time),3), dtype='f')
       dataset1[:,0] = time
-      dataset1[:,1] = np.real(strain)
-      dataset1[:,2] = np.imag(strain)
+      dataset1[:,1] = np.real(psi4)
+      dataset1[:,2] = np.imag(psi4)
 
       dataset2[:,0] = time
-      dataset2[:,1] = np.real(delta_strain)
-      dataset2[:,2] = np.imag(delta_strain)
+      dataset2[:,1] = np.real(delta_psi4)
+      dataset2[:,2] = np.imag(delta_psi4)
 
-  return time, strain, delta_strain
-
-    
-def MakeJSonFile(data_dir):
-  a = sxs.metadata.Metadata()
-  b = a.from_txt_file(data_dir + 'metadata.txt')
+  return time, psi4, delta_psi4
 
 def main():
   p = argparse.ArgumentParser(description="Generate EDGB waveform from given EDGB simulation")
@@ -221,14 +153,9 @@ def main():
 
   print("\n Working in directory " + data_dir + "\n")
 
-  ## First make the json file
-  #print("Making metadata.json")
-  #MakeJSonFile(data_dir + '/')
-
-  ## produce DeltaStrain.h5 and BackgroundStrain.h5 files
-  print("\n Computing and outputting delta strain \n")
-  OutputEDGBDeltaStrain(data_dir, args.only22)
-
+  ## produce DeltaPsi4.h5 and BackgroundPsi4.h5 files
+  print("\n Computing and outputting delta psi4 \n")
+  OutputEDGBPsi4(data_dir, args.only22)
 
 if __name__ == "__main__":
   main()
